85a44f3c5606dcc6d8fe2c1623a82abb5b48ffea
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-01 15:04:28 +0900

    회원가입 페이지/컨트롤러 작성

---
+++ src/main/java/kimch321/spring4/semiprojectv4/controller/joinController.java
@@ -0,0 +1,29 @@
+package kimch321.spring4.semiprojectv4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/join")
+public class joinController {
+    @GetMapping("/agree")
+    public String agree() {
+        return "join/agree.tiles";
+    }
+
+    @GetMapping("/checkme")
+    public String checkme() {
+        return "join/checkme.tiles";
+    }
+
+    @GetMapping("/joinme")
+    public String joinme() {
+        return "join/joinme.tiles";
+    }
+
+    @GetMapping("/joinok")
+    public String joinok() {
+        return "join/joinok.tiles";
+    }
+}
---
+++ src/main/webapp/resources/js/semiprojectv4.js
@@ -0,0 +1,6 @@
+const logo = document.querySelector('#logo');
+
+logo.addEventListener('click', function (){
+    console.log('hi')
+    location.href = '/';
+})
