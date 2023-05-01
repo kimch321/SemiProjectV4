69192a1afb96c8e923a9efccc07382707474a5d0
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-01 15:44:20 +0900

    회원가입 - 이용약관 페이지

--- src/main/webapp/resources/js/semiprojectv4.js
+++ src/main/webapp/resources/js/semiprojectv4.js
@@ -1,6 +1,26 @@
+// header 로고 클릭 이벤트
 const logo = document.querySelector('#logo');
-
-logo.addEventListener('click', function (){
-    console.log('hi')
+logo.addEventListener('click', () => {
     location.href = '/';
+})
+
+// agree
+// 이용약관 체크박스
+const agree1 = document.querySelector('#agree1');
+
+// 개인정보 이용 체크박스
+const agree2 = document.querySelector('#agree2');
+
+// 동의 버튼
+const agreeOK = document.querySelector('#agreeOk');
+agreeOK?.addEventListener('click', () => {
+    if(!agree1.checked) alert("이용약관 동의에 체크해주세요");
+    else if(!agree2.checked) alert("개인정보 이용 동의에 체크해주세요.");
+    else location.href = "/join/checkme"
+})
+
+// 미동의 버튼
+const agreeNo = document.querySelector('#agreeNo');
+agreeNo?.addEventListener('click',() => {
+    location.href = "/";
 })
