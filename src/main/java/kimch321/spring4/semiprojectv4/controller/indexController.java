28194f3aa41bf68016e8a5858e109be84c95dfb4
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-01 11:37:28 +0900

    bootstrap5 라이브러리 및 타일즈3 적용 확인

---
+++ src/main/java/kimch321/spring4/semiprojectv4/controller/indexController.java
@@ -0,0 +1,12 @@
+package kimch321.spring4.semiprojectv4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+    @GetMapping("/")
+    public String index() {
+        return "index.tiles";
+    }
+}
