47c4f5b3d573aaa71a7501dedea762576d3de34c
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-02 11:15:52 +0900

    회원가입 - 우편번호검색 RESTful 1 - controller

--- src/main/java/kimch321/spring4/semiprojectv4/controller/joinController.java
+++ src/main/java/kimch321/spring4/semiprojectv4/controller/joinController.java
@@ -1,13 +1,21 @@
 package kimch321.spring4.semiprojectv4.controller;
 
 import kimch321.spring4.semiprojectv4.model.Member;
+import kimch321.spring4.semiprojectv4.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/join")
 public class joinController {
+
+    @Autowired private JoinService jnsrv;
+
     @GetMapping("/agree")
     public String agree() {
         return "join/agree.tiles";
@@ -40,4 +48,24 @@ public class joinController {
     public String joinok() {
         return "join/joinok.tiles";
     }
+
+    // 우편번호 검색
+    // /join/zipcode?dong=동이름
+    // 검색결과는 뷰페이지 없이 바로 응답으로 출력 : RESTful 방식
+    // 서블릿에서 제공하는 HttpServletResponse 객체를 이용하면 스프링의 뷰리졸버 없이 바로 응답으로 출력할 수 있음
+    // 단, 응답유형은 JSON 형식으로 함
+    @ResponseBody
+    @GetMapping("/zipcode")
+    public void zipcode(String dong, HttpServletResponse res) {
+        try {
+            // 응답유형은 JSON으로 설정
+            res.setContentType("application/json; charset=utf-8");
+            // 검색된 우편번호 결과를 뷰없이 바로 응답으로 출력
+            res.getWriter().print(jnsrv.findZipcode(dong));
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
 }
